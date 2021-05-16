9656fccfef38a51ee4d74190785457958ae2781a
Zayne <llz981011@hotmail.com>
2021-05-16 04:27:53 +0100

    Initial Commit

---
+++ Shared/LibrarianApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibrarianApp.swift
+//  Shared
+//
+//  Created by Zayne on 16/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct LibrarianApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
