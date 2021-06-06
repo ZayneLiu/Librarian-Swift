621687723c30e68b9c3cf09073fb12d178ddd194
Zayne <llz981011@hotmail.com>
2021-06-06 17:34:38 +0100

    Added delete button to remove indexed folder.

--- Shared/Views/Components/FolderItem.swift
+++ Shared/Views/Components/FolderItem.swift
@@ -6,36 +6,59 @@ import Foundation
 import SwiftUI
 
 struct FolderItem: View {
-	@State var isOn: Bool = true
-	@State var isSheetPresented: Bool = false
-
-	var folder: SelectedFolder
-
-	init(folder: SelectedFolder) {
+	init(folder: Folder) {
 		self.folder = folder
 	}
+
+	@State var isOn: Bool = true
+	@State var isSheetPresented: Bool = false
+	@State var isHovered: Bool = false
+
+	var folder: Folder
 
 	var body: some View {
 		HStack {
 			Toggle(isOn: $isOn) {
-				#if os(macOS)
-					Button(action: { isSheetPresented = true }) {
-						Image(systemName: "info.circle").foregroundColor(.blue)
-					}.buttonStyle(PlainButtonStyle())
-				#endif
-
-				Image(systemName: "folder")
-				Text("\(folder.name)")
-
 				#if os(iOS)
 					Button(action: { isSheetPresented = true }) {
 						Image(systemName: "info.circle").foregroundColor(.blue)
 					}.buttonStyle(PlainButtonStyle())
 				#endif
 
-			}.popover(isPresented: $isSheetPresented, content: {
-				Text("\(folder.url.path)").padding()
+				Image(systemName: "folder.fill")
+				Text("\(folder.name)")
+					.font(.system(size: 13))
+					.lineLimit(1)
+					.frame(minHeight: 18)
+
+				#if os(macOS)
+					Spacer()
+					// Button: Remove folder from index database
+					if isHovered {
+						Button(action: { removeIndexedFolder() }) {
+							Image(systemName: "trash.fill").foregroundColor(.red)
+						}.buttonStyle(PlainButtonStyle())
+					}
+
+					// Button: Preview folder location
+					Button(action: { isSheetPresented = true }) {
+						Image(systemName: "info.circle").foregroundColor(.blue)
+					}.buttonStyle(PlainButtonStyle())
+				#endif
+			}
+			// Detail popover
+			.popover(isPresented: $isSheetPresented, content: {
+				Text("Path: `\(folder.url.path)`").padding()
 			})
-		}
+		}.onHover(perform: { hovered in isHovered = hovered })
+	}
+
+	func getWidth() -> CGFloat {
+		CGFloat(isHovered ? 100 : 0)
+	}
+
+	func removeIndexedFolder() {
+		FolderViewModel.model.removeFolder(folder: folder)
+		print(FolderViewModel.model.folders)
 	}
 }
