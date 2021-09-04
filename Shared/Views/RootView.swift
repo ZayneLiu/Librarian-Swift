eee5fe7ebfa29bb4de7063c5678f1002c160f31c
Zayne Liu <zayneliu@hotmail.com>
2021-09-04 11:45:20 +0100

    Update RootView.swift

--- Shared/Views/RootView.swift
+++ Shared/Views/RootView.swift
@@ -18,34 +18,37 @@ struct RootView: View {
 
 	var body: some View {
 		NavigationView {
-			VStack(content: {
-				#if os(macOS)
+			VStack(alignment: .center, spacing: 10, content: {
+//				#if os(macOS)
+				HStack {
 					Text("\(Image(systemName: "books.vertical.fill")) Librarian")
 						.font(.title)
-				#endif
+				}.frame(height: TOP_BAR_HEIGHT, alignment: .center)
+//				#endif
 
-				FolderView()
+				Divider()
 
-//				NavigationLink(destination: FolderView().navigationTitle("Folders")) {
-//					HStack {
-//						Image(systemName: "folder.badge.plus")
-//							.frame(width: 25, alignment: .center)
-//						Text("Folders")
-//					}.font(.title2)
-//				}
-//
-//				NavigationLink(destination: SearchView().navigationTitle("Search")) {
-//					HStack {
-				////						Image(systemName: "doc.text.magnifyingglass")
-//						Image(systemName: "magnifyingglass")
-//							.frame(width: 25, alignment: .center)
-//						Text("Search")
-//					}.font(.title2)
-//				}
+				NavigationLink(destination: FolderView().navigationTitle("Folders")) {
+					HStack {
+						Image(systemName: "folder.badge.plus")
+							.frame(width: 25, alignment: .center)
+						Text("Folders")
+					}.font(.title2)
+				}.buttonStyle(.plain)
 
+				NavigationLink(destination: SearchView().navigationTitle("Search")) {
+					HStack {
+						// Image(systemName: "doc.text.magnifyingglass")
+						Image(systemName: "magnifyingglass")
+							.frame(width: 25, alignment: .center)
+						Text("Search")
+					}.font(.title2)
+				}.buttonStyle(.plain)
+				Spacer()
 			})
+				.padding()
 			#if os(iOS)
-				.navigationBarTitle("Folder")
+				.navigationBarTitle("Menu")
 			#endif
 
 			SearchView().navigationTitle("Search")
