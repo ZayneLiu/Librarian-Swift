02ed47ef804ec50939208e0264a1f848a676cafa
Zayne Liu <zayneliu@hotmail.com>
2021-09-04 02:32:53 +0100

    Update SearchView.swift

--- Shared/Views/SearchView.swift
+++ Shared/Views/SearchView.swift
@@ -5,43 +5,46 @@
 //  Created by Zayne on 02/06/2021.
 //
 
+import Custodian
 import SwiftUI
 
 struct SearchView: View {
 	@ObservedObject var model = SearchViewModel.model
+	@ObservedObject var folderModel = FolderViewModel.model
 
 	var body: some View {
 		VStack(spacing: 10, content: {
 			VStack(content: {
 				HStack(content: {
-					Text("Search Keyword:")
-					TextField("Input", text: $model.text_input)
-						.frame(width: 150, alignment: .center)
+					TextField("Keyword", text: $model.text_input)
+						.disableAutocorrection(true)
+						.textFieldStyle(RoundedBorderTextFieldStyle())
+						.frame(width: 200, alignment: .center)
+					#if os(iOS)
+						.autocapitalization(.none)
+					#endif
 
 					Button("Search") {
-						print(model.text_input)
-						#warning("TODO: Call search API")
+						let searchRes = folderModel.search(keyword: model.text_input)
+						model.resultList = searchRes
 					}
 				})
 			}).frame(maxHeight: TOP_BAR_HEIGHT)
+
 			Divider()
 
-			HStack {
+			HStack(content: {
 				Text("Search Result:")
-				Text("Found \(model.resultList.count) file(s)")
-					.font(.subheadline)
-					.italic()
-			}
-
-			List(content: {
-				// List of search result
-				ForEach(model.resultList) { result in
-					// Search Result File item
-					#warning("Search Result File item")
-					SearchResultItem(result: SearchResult(searchResult: result))
-				}
+				Text("Found \(model.resultList.count) file(s)").font(.subheadline).italic()
 			})
-		}).frame(alignment: .top)
+
+			List {
+				// List of search result
+				ForEach(model.resultList, id: \.id) { result in
+					SearchResultItem(result: result)
+				}
+			}.padding(0)
+		}).frame(alignment: .top).padding()
 	}
 }
 
