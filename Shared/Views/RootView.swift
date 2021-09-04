5c8a4842786e38474bda49aa42b8f91f3c90a535
Zayne Liu <zayneliu@hotmail.com>
2021-09-04 02:33:02 +0100

    Update RootView.swift

--- Shared/Views/RootView.swift
+++ Shared/Views/RootView.swift
@@ -5,21 +5,51 @@
 //  Created by Zayne on 16/05/2021.
 //
 
+import Foundation
 import SwiftUI
 
 var TOP_BAR_HEIGHT: CGFloat = 36.0
+class EnvironmentObj: ObservableObject {
+	@Published var isActive = true
+}
 
 struct RootView: View {
-	var body: some View {
-		VStack(content: {
-			NavigationView(content: {
-				FolderView()
-				SearchView()
-			}).frame(idealWidth: 600,
-			         minHeight: 500,
-			         alignment: .center)
+	@ObservedObject var envObj = EnvironmentObj()
 
-		}).frame(alignment: .top)
+	var body: some View {
+		NavigationView {
+			VStack(content: {
+				#if os(macOS)
+					Text("\(Image(systemName: "books.vertical.fill")) Librarian")
+						.font(.title)
+				#endif
+
+				FolderView()
+
+//				NavigationLink(destination: FolderView().navigationTitle("Folders")) {
+//					HStack {
+//						Image(systemName: "folder.badge.plus")
+//							.frame(width: 25, alignment: .center)
+//						Text("Folders")
+//					}.font(.title2)
+//				}
+//
+//				NavigationLink(destination: SearchView().navigationTitle("Search")) {
+//					HStack {
+				////						Image(systemName: "doc.text.magnifyingglass")
+//						Image(systemName: "magnifyingglass")
+//							.frame(width: 25, alignment: .center)
+//						Text("Search")
+//					}.font(.title2)
+//				}
+
+			})
+			#if os(iOS)
+				.navigationBarTitle("Librarian")
+			#endif
+
+			SearchView().navigationTitle("Search")
+		}
 	}
 }
 
