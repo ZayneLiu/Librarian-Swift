0e36589b55a9a12fb036a9af89b0ace98c965b9e
Zayne Liu <zayneliu@hotmail.com>
2021-11-27 16:15:01 +0000

    Update FolderItem.swift

--- Shared/Views/Components/FolderItem.swift
+++ Shared/Views/Components/FolderItem.swift
@@ -12,50 +12,66 @@ struct FolderItem: View {
 
 	@State var isOn: Bool = true
 	@State var isSheetPresented: Bool = false
-	@State var isHovered: Bool = false
-
 	var folder: FolderModel
-
 	var body: some View {
-		HStack {
+		VStack {
 			Toggle(isOn: $isOn) {
-				#if os(iOS)
-					Button(action: { isSheetPresented = true }) {
-						Image(systemName: "info.circle").foregroundColor(.blue)
-					}.buttonStyle(PlainButtonStyle())
-				#endif
-
-				Image(systemName: "folder.fill")
-				Text("\(folder.name)")
-					.font(.system(size: 13))
-					.lineLimit(1)
-					.frame(minHeight: 18)
-
-				#if os(macOS)
-					// Button: Remove folder from index database
-					if isHovered {
-						Button(action: { removeIndexedFolder() }) {
-							Image(systemName: "trash.fill").foregroundColor(.red)
+				HStack(alignment: .top) {
+					#if os(iOS)
+						Button(action: { isSheetPresented = true }) {
+							Image(systemName: "info.circle").foregroundColor(.blue)
 						}.buttonStyle(PlainButtonStyle())
-					}
+					#endif
 
-					// Button: Preview folder location
-					Button(action: { isSheetPresented = true }) {
-						Image(systemName: "info.circle").foregroundColor(.blue)
-					}.buttonStyle(PlainButtonStyle())
-				#endif
+					Text("\(Image(systemName: "folder.fill")) \(folder.name)").font(.body).lineLimit(1)
+
+					// #if os(macOS)
+					// 	// Button: Remove folder from index database
+					// 	if isHovered {
+					// 		Button(action: { removeIndexedFolder() }) {
+					// 			Image(systemName: "trash.fill").foregroundColor(.red)
+					// 		}.buttonStyle(PlainButtonStyle())
+					// 	}
+					//
+					// 	// Button: Preview folder location
+					// 	Button(action: { isSheetPresented = true }) {
+					// 		Image(systemName: "info.circle").foregroundColor(.blue)
+					// 	}.buttonStyle(PlainButtonStyle())
+					// #endif
+				}
 			}
-			// Detail popover
-			.popover(isPresented: $isSheetPresented, content: {
+			Divider().padding(0)
+			DisclosureGroup("Detail") {
+				HStack(alignment: .center) {
+					VStack(alignment: .leading, content: {
+						#warning("deatils of given folder")
+						VStack(alignment: .leading, content: {
+							HStack {
+								Text("Total file:")
+								Spacer()
+								Text("\(folder.folder.files.count)")
+							}
+							HStack {
+								Text("File types:")
+								Spacer()
+								Text("\(folder.getFileTypes())")
+								//	TODO: summary of supported files
+							}
+						})
+					})
+
+					Spacer()
+				}
+			}.padding(.vertical, 0).padding(.horizontal, 30).popover(isPresented: $isSheetPresented, content: {
+				// Detail popover
 				Text("Path: `\(folder.url.path)`").padding()
 			})
-		}.onHover(perform: { hovered in isHovered = hovered })
+		}
+		// .onHover(perform: { hovered in isHovered = hovered })
 	}
-
-	func getWidth() -> CGFloat {
-		CGFloat(isHovered ? 100 : 0)
-	}
-
+	// func getWidth() -> CGFloat {
+	// 	CGFloat(isHovered ? 100 : 0)
+	// }
 	func removeIndexedFolder() {
 		FolderViewModel.model.removeFolder(folder: folder)
 		print(FolderViewModel.model.folders)
